5bf4c00cff80cfc4baac0111288e3065ce443a02
Maksym Babichenko <nestky2@gmail.com>
2025-02-19 15:10:59 +0200

    lesson-1-2-1

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -5,5 +5,32 @@
   Например, факториал числа 3 это 1 * 2 * 3 (6).
 */
 
+// 10 = 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1 = 3 628 800
+// 5 = 5 * 4 * 3 * 2 * 1 = 120
+// 5 = 1 * 2 * 3 * 4 * 5 = 120
+
+// n = n * (n - 1) * (n - 2) * (n - 3) ...
+
+// 10 = 10 * (10 - 1); Iteration 1
+// 10 = 9 * (10 - 2); Iteration 2
+
+// "==" "===" "!=="
+
 // your code
-let factorial
+let factorial = 10;
+
+// let maxIteration = 10; // Максимальное количество итераций
+// let currentIteration = 1; // Текущая итерация
+
+let n = 10; // текущее число для того что бы узнать следующее число для факториала
+
+while (n > 1) {
+  console.log("BEFORE calculation", factorial);
+  console.log(`calculation params: ${factorial} * (${n} - 1)`);
+  factorial = factorial * (n - 1);
+  console.log("AFTER calculation", factorial);
+  console.log("n", n);
+  n = n - 1;
+}
+
+// console.log(factorial);
