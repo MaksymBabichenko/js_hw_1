5d280d29bb84a299ea7c29da523fc6da7d494bc8
Maksym Babichenko <nestky2@gmail.com>
2025-03-04 16:35:45 +0200

    01_02_03

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -14,7 +14,7 @@ if (score >= 90 && score <= 100) {
 } else if (score >= 80 && score <= 89) {
   grade = "B";
 } else if (score >= 70 && score <= 79) {
-  grade = "С";
+  grade = "C";
 } else if (score >= 50 && score <= 69) {
   grade = "D";
 } else if (score >= 0 && score <= 49) {
