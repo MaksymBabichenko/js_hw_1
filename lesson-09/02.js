ac714419adcd9df3469eb7435beb010e8dfebcdb
Maksym Babichenko <nestky2@gmail.com>
2025-04-30 13:52:44 +0300

    09-02

--- lesson-09/02.js
+++ lesson-09/02.js
@@ -24,4 +24,17 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+const map = (array, callback) => {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    const el = array[i];
+    
+      result.push(callback(el,i));
+  }
+  return result;
+};
+console.log(
+  map([1, 3, 4, 5, 6], (el) => {
+    return el * 2;
+  })
+);
